485629a15c3949c494acce2e6ad96122fabe7203
Bhavya Bhalla <91721455+BhavyaBhalla27@users.noreply.github.com>
2022-03-05 06:56:05 +0530

    Friend Functions

---
+++ OBJECT ORIENTED PROGRAMMING/Friend_Functions.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+    int a, b;
+
+public:
+    void setNumber(int n1, int n2)
+    {
+        a = n1;
+        b = n2;
+    }
+    friend Complex sumComplex(Complex o1, Complex o2);           //friend function  
+    void printNumber()
+    {
+        cout << "Your number is " << a << "+" << b << "i" << endl;
+    }
+};
+Complex sumComplex(Complex o1, Complex o2)
+{
+    Complex o3;
+    o3.setNumber((o1.a + o2.a), (o2.a + o2.b));
+    return o3;
+}
+int main()
+{
+    Complex c1, c2, c3;
+    c1.setNumber(1, 4);
+    c1.printNumber();
+    c2.setNumber(5, 8);
+    c2.printNumber();
+    c3 = sumComplex(c1, c2);
+    c3.printNumber();
+    return 0;
+}
