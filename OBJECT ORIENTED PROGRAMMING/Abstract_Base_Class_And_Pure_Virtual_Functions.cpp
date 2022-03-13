73fe7fc5d5b17c7818bf75a3dbbc9bc4e6ddf244
Bhavya Bhalla <91721455+BhavyaBhalla27@users.noreply.github.com>
2022-03-13 13:45:37 +0530

    Pure Virtual Function and Abstract Base Class

---
+++ OBJECT ORIENTED PROGRAMMING/Abstract_Base_Class_And_Pure_Virtual_Functions.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+class CWH{                              // This is an Abstract Base Class...
+    protected:
+        string title;
+        float rating;
+    public:
+        CWH(string s,float r){
+            title = s;
+            rating =r;
+        }
+        virtual void display() = 0;     // Pure Virtual Function...Do Nothing Function
+};
+class CWHVideo:public CWH{
+    int videoLength;
+    public:
+        CWHVideo(string s,float r,int VL): CWH(s,r){
+            videoLength = VL;
+        }
+        void display(){
+            cout<<"This is an amazing video with title "<<title<<endl;
+            cout<<"Ratings: "<<rating<<" out of 5 stars"<<endl;
+            cout<<"Length of this video is: "<<videoLength<<" minutes"<<endl;
+        }
+};
+class CWHText:public CWH{
+    int words;
+    public:
+        CWHText(string s,float r,int wc): CWH(s,r){
+            words = wc;
+        }
+        void display(){
+            cout<<"This is a text tutorial with title "<<title<<endl;
+            cout<<"Ratings: "<<rating<<" out of 5 stars"<<endl;
+            cout<<"No of words : "<<words<<" words"<<endl;
+        }
+};
+int main(){
+    string title = new char[30];
+    float rating,vlen;
+    int words;
+    title = "Django";
+    vlen = 4.56;
+    rating = 4;
+    CWHVideo djvideo(title,rating,vlen);
+    title = "Django";
+    rating = 4.5;
+    words = 456;
+    CWHText  djtext(title,rating,words);
+    CWH *tuts[2];
+    tuts[0]=&djvideo;
+    tuts[1]=&djtext;
+    tuts[0]->display();          // This would run the display function of base class CWH if virtual was not written...
+    tuts[1]->display();          // This would run the display function of base class CWH if virtual was not written...
+    return 0;
+}
